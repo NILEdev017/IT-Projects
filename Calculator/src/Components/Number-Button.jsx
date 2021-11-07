9a54949aecb92ff1ed202d83ff1e656cd10f2660
NILEdev017 <74454443+NILEdev017@users.noreply.github.com>
2021-11-07 17:54:39 +0100

    Delete Calculator directory

--- Calculator/src/Components/Number-Button.jsx
+++
@@ -1,9 +0,0 @@
-import React from "react";
-
-export default function NumberButton({ handleClick, num }) {
-  return (
-    <button onClick={() => handleClick(num)} className="number-button">
-      {num}
-    </button>
-  );
-}
